6fa5f6f5fff2f7f8dd18ced4395013199c3195c5
YUAN <yankees2484@gmail.com>
2020-11-21 04:02:56 +0800

    checkboxlist

---
+++ prjMVCCheckBoxList/MVCCheckBoxList/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using MVCCheckBoxList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCheckBoxList.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            ProductModel product = new ProductModel();
+            using (DbModels db = new DbModels())
+            {
+                product.Products = db.tProducts.ToList<tProduct>();
+            }
+            return View(product);
+
+        }
+        [HttpPost]
+        public ActionResult Index(ProductModel model)
+        {
+            var selectedProduct = model.Products.Where(a => a.IsChecked == true).ToList<tProduct>();
+            //建立內容物件
+            return Content(string.Join("-",selectedProduct.Select(b=>b.fProductName)));
+
+        }
+    }
+}

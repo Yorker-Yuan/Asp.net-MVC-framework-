efd94f80a1312254ecf56f3cf7f66746b4fbaefa
YUAN <yankees2484@gmail.com>
2020-11-21 04:17:07 +0800

    database

---
+++ prjDropDownList/DropDownList/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DropDownList.Startup))]
+namespace DropDownList
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}

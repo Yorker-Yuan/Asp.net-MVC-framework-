25e4faf4e30fadb80d9fe0f618b6759ae34e8780
YUAN <yankees2484@gmail.com>
2020-11-22 03:27:55 +0800

    MVCCRUD

---
+++ prjMVCCRUD/MVCCRUD/Controllers/CustomerController.cs
@@ -0,0 +1,122 @@
+﻿using MVCCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Services.Client;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCRUD.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            using (dbMVCCRUDEntities db = new dbMVCCRUDEntities()) 
+            {
+                var details = db.tCustomers.ToList();
+                return View(details);
+
+            }
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            using (dbMVCCRUDEntities db = new dbMVCCRUDEntities())
+            {
+                var details = db.tCustomers.Where(a=>a.fId == id).FirstOrDefault();
+                return View(details);
+            }
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(tCustomer customer)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                using (dbMVCCRUDEntities db = new dbMVCCRUDEntities())
+                {
+                    db.tCustomers.Add(customer);
+                    db.SaveChanges();
+                }
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (dbMVCCRUDEntities db = new dbMVCCRUDEntities())
+            {
+                var details = db.tCustomers.Where(a => a.fId == id).FirstOrDefault();
+                return View(details);
+            }
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, tCustomer customer)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                using (dbMVCCRUDEntities db = new dbMVCCRUDEntities())
+                {
+                    db.Entry(customer).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (dbMVCCRUDEntities db = new dbMVCCRUDEntities())
+            {
+                var details = db.tCustomers.Where(a => a.fId == id).FirstOrDefault();
+                return View(details);
+            }
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                using (dbMVCCRUDEntities db = new dbMVCCRUDEntities())
+                {
+                    tCustomer customer = db.tCustomers.Where(a => a.fId == id).FirstOrDefault();
+                    db.tCustomers.Remove(customer);
+                    db.SaveChanges();
+                }
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ prjUserSystem/UserSystem/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserSystem.Models;
+
+namespace UserSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Autherize(tUserLogin user)
+        {
+            using (dbUserRegistationEntities db = new dbUserRegistationEntities())
+            {
+                var details = db.tUserLogins.Where(a => a.fName == user.fName && a.fPassword == user.fPassword).FirstOrDefault();
+                if (details == null)
+                {
+                    user.LoginErrorMsg = "帳號或密碼錯誤";
+                    return View("Index", user);
+                }
+                else
+                {
+                    Session["UserId"] = details.fUserId;
+                    Session["UserName"] = details.fName;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            int userId = (int)Session["UserId"];
+            Session.Abandon();
+            return RedirectToAction("Index","Login");
+        }
+    }
+}

0bc9e61cc19092b83f78278dbefd6dbffdb47333
YUAN <yankees2484@gmail.com>
2020-11-22 20:22:55 +0800

    mvccrud 系統

---
+++ prjJQueryAjaxInAspNetMVC/JQueryAjaxInAspNetMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,99 @@
+﻿using JQueryAjaxInAspNetMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace JQueryAjaxInAspNetMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAll()
+        {
+            return View(GetAllEmployee());
+        }
+
+        IEnumerable<tEmployee> GetAllEmployee()
+        {
+            using (dbJQueryAjaxMVCEntities db = new dbJQueryAjaxMVCEntities())
+            {
+                return db.tEmployees.ToList<tEmployee>();
+            }
+        }
+
+        public ActionResult AddOrEdit(int id= 0)
+        {
+            tEmployee t = new tEmployee();
+            if (id != 0)
+            {
+                using (dbJQueryAjaxMVCEntities db = new dbJQueryAjaxMVCEntities())
+                {
+                    t = db.tEmployees.Where(a => a.fEmpId == id).FirstOrDefault<tEmployee>();
+                }
+            }
+            return View(t);
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(tEmployee emp)
+        {
+            try
+            {
+                if (emp.fImageUpload != null)
+                {
+                    string filename = Path.GetFileNameWithoutExtension(emp.fImageUpload.FileName);
+                    string extension = Path.GetExtension(emp.fImageUpload.FileName);
+                    filename = filename + DateTime.Now.ToString("yymmssfff") + extension;
+                    emp.fImagePath = "~/AppFiles/images/" + filename;
+                    emp.fImageUpload.SaveAs(Path.Combine(Server.MapPath("~/AppFiles/images/"), filename));
+                }
+                using (dbJQueryAjaxMVCEntities db = new dbJQueryAjaxMVCEntities())
+                {
+                    if (emp.fEmpId == 0)
+                    {
+                        db.tEmployees.Add(emp);
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        db.Entry(emp).State = EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                }
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployee()), message = "成功新增!!" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+
+                return Json(new { success = false,message = e.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (dbJQueryAjaxMVCEntities db = new dbJQueryAjaxMVCEntities())
+                {
+                    tEmployee t = db.tEmployees.Where(a => a.fEmpId == id).FirstOrDefault<tEmployee>();
+                    db.tEmployees.Remove(t);
+                    db.SaveChanges();
+                }
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployee()), message = "成功刪除!!" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+
+                return Json(new { success = false, message = e.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ prjJQueryAjaxInAspNetMVC/JQueryAjaxInAspNetMVC/Scripts/Scripts.js
@@ -0,0 +1,95 @@
+﻿$(function () {
+    $('#loaderbody').addClass('hide');
+
+    $(document).bind('ajaxStart', function () {
+        $('#loaderbody').removeClass('hide');
+    }).bind('ajaxStop', function () {
+        $('#loaderbody').addClass('hide');
+    });
+});
+
+
+function ShowImagePreview(imagesUploader, previewImage) {
+    if (imagesUploader.files && imagesUploader.files[0]) {
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            $(previewImage).attr('src', e.target.result);
+        }
+        reader.readAsDataURL(imagesUploader.files[0]);
+    }
+}
+
+function jQueryAjaxPost(form) {
+    $.validator.unobtrusive.parse(form);
+    if ($(form).valid()) {
+        let ajaxConfig = {
+            type: 'POST',
+            url: form.action,
+            data: new FormData(form),   /*表單取值，若沒有則無法送出表單的值*/
+            success: function (res) {
+                if (res.success) {
+                    $('#firstTab').html(res.html);
+                    refreshAddNewTab($(form).attr('data_resetUrl'), true);
+                    $.notify(res.message, "success");
+                    if (typeof activateJQueryTable !== 'undefined' && $.isFunction(activateJQueryTable))
+                        activateJQueryTable();
+                } else {
+                    //err msg
+                    $.notify(res.message, "error");
+                }
+            }
+        }
+        if ($(form).attr('enctype') == "multipart/form-data") {
+            ajaxConfig["contentType"] = false;
+            ajaxConfig["processData"] = false;
+        }
+        $.ajax(ajaxConfig);
+    }
+    return false;
+}
+
+function refreshAddNewTab(resetUrl, showViewTab) {
+    $.ajax({
+        type: 'GET',
+        url: resetUrl,
+        success: function (res) {
+            $('#secondTab').html(res);
+            $('ul.nav.nav-tabs a:eq(1)').html('Add New');
+            if (showViewTab)
+                $('ul.nav.nav-tabs a:eq(0)').tab('show');
+        }
+    })
+}
+
+function Edit(url) {
+    $.ajax({
+        type: 'GET',
+        url: url,
+        success: function (res) {
+            $('#secondTab').html(res);
+            $('ul.nav.nav-tabs a:eq(1)').html('Edit');
+                $('ul.nav.nav-tabs a:eq(1)').tab('show');
+        }
+    })
+}
+
+function Delete(url) {
+    if (confirm('確定要刪除嗎?') == true) {
+
+        $.ajax({
+            type: 'POST',
+            url: url,
+            success: function (res) {
+                if (res.success) {
+                    $('#firstTab').html(res.html);
+                    $.notify(res.message, "warn");
+                    if (typeof activateJQueryTable !== 'undefined' && $.isFunction(activateJQueryTable))
+                        activateJQueryTable();
+                }
+                else {
+                    $.notify(res.message, "error");
+                }
+            }
+        })
+    }
+}
